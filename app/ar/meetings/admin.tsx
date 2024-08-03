f9a140efb121835cef2e94f92bac224cf7dba159
Mohamed Belal <150057664+MohamedBelal-11@users.noreply.github.com>
2024-08-03 21:51:45 +0000

    fix the nested dates in

--- app/ar/meetings/admin.tsx
+++ app/ar/meetings/admin.tsx
@@ -1,5 +1,9 @@
+"use client";
+import LoadingDiv from "@/app/components/loadingDiv";
+import { useState } from "react";
+
 interface Meet {
-  status: "didnt_start" | ""
+  status: "didnt_start" | "didnt_checked" | "checked";
 }
 
 type Responset =
@@ -12,8 +16,30 @@ type Responset =
     }
   | null;
 
-
 const AdminContent: React.FC<{ isSuper: boolean }> = ({ isSuper }) => {
+  const [response, setResponse] = useState<Responset>();
+
+  if (response === undefined) {
+    return <LoadingDiv loading />;
+  }
+
+  if (response === null) {
+    return <></>;
+  }
+
+  if (!response.is_accepted) {
+  }
+
+  if (!response.is_accepted) {
+    return (
+      <div className="p-4">
+        <p className="p-6 bg-white rounded-lg text-gray-600">
+          لم يتم الموافقة عليك بعد لذلك لا يمكنك دخول هذه الصفحة بعد
+        </p>
+      </div>
+    );
+  }
+
   return <></>;
 };
 
