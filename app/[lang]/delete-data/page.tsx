3c5cd15d7658f8dfd6384c693fa076c979e13df8
Mohamed Belal <150057664+MohamedBelal-11@users.noreply.github.com>
2025-06-22 14:49:29 +0000

    feat: add delete data page with contact information in Arabic

---
+++ app/[lang]/delete-data/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "طريقة حذف بياناتك - أكاديمية تزود",
+};
+
+const Page: React.FC = () => {
+  return (
+    <>
+      <div className="h-px"></div>
+      <main className="sm:m-12 m-4 sm:p-8 p-3 ps-8 rounded-lg bg-white">
+        التواصل مع (+201060512152)
+      </main>
+    </>
+  );
+};
+
+export default Page;
--- app/[lang]/privacy-policy/page.tsx
+++ app/[lang]/privacy-policy/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "دليل الطالب - أكاديمية تزود",
+  title: "سياسة الخصوصية - أكاديمية تزود",
 };
 
 const Page: React.FC = () => {
